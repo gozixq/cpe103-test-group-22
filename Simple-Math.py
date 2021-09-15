b1bcbd99dfc287711c6fee55b109edfc73234809
teeraphat147258 <teeraphat147258@gmail.com>
2021-09-15 17:29:43 +0700

    Revert "การคูณ"
    This reverts commit 79fe32189de172027b21305573f33a32354d6761.

--- Simple-Math.py
+++ Simple-Math.py
@@ -1,7 +1,9 @@
-def multiply(a,b):
-    return(a*b)
+def add(a,b):
+    return(a+b)
+def subtract(a,b):
+    return(a-b)
 
-i = multiply(i, 5)
-
-
+i = 0
+i = add (i, 3)
+i = subtract(i, 1)
 print("i = " + str(i))
