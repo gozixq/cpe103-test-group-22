79fe32189de172027b21305573f33a32354d6761
teeraphat147258 <teeraphat147258@gmail.com>
2021-09-15 17:29:26 +0700

    การคูณ
    การคูณ

--- Simple-Math.py
+++ Simple-Math.py
@@ -1,9 +1,7 @@
-def add(a,b):
-    return(a+b)
-def subtract(a,b):
-    return(a-b)
+def multiply(a,b):
+    return(a*b)
 
-i = 0
-i = add (i, 3)
-i = subtract(i, 1)
+i = multiply(i, 5)
+
+
 print("i = " + str(i))
